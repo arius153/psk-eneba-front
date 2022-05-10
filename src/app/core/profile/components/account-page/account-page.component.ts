44c08bfcea0f2756b62279f0fc76f3dc1102d99a
Airidas Kutra <airidas.kutra@mif.stud.vu.lt>
2022-05-10 14:01:14 +0300

    Finished mock version of account page

--- src/app/core/profile/components/account-page/account-page.component.ts
+++ src/app/core/profile/components/account-page/account-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserFull} from '../../../../shared/models/user-full';
 
 @Component({
   selector: 'app-account-page',
@@ -9,7 +10,49 @@ export class AccountPageComponent implements OnInit {
 
   constructor() { }
 
+  user: UserFull;
+
+  // Phone number changing
+  newPhoneNumber: string;
+  changingNumber = false;
+  showChangeNumber(): void {
+    this.changingNumber = true;
+    this.newPhoneNumber = '';
+  }
+  changeNumber(): void {
+    this.user.phoneNumber = this.newPhoneNumber;
+    this.changingNumber = false;
+  }
+  cancelNumberChange(): void {
+    this.changingNumber = false;
+  }
+
+  // Password changing
+  newPassword: string;
+  newPasswordRepeated: string;
+  changingPassword = false;
+  showChangePassword(): void {
+    this.changingPassword = true;
+    this.newPassword = '';
+    this.newPasswordRepeated = '';
+  }
+  changePassword(): void {
+    if (this.newPassword === this.newPasswordRepeated) {
+      this.user.password = this.newPassword;
+      this.changingPassword = false;
+    }
+  }
+  cancelPasswordChange(): void {
+    this.changingPassword = false;
+  }
+
+
   ngOnInit(): void {
+    this.user = new UserFull();
+    this.user.fullName = 'Bronius Barzdauskas';
+    this.user.natIdNumber = '59707271234';
+    this.user.email = 'meskiukas@one.lt';
+    this.user.phoneNumber = '+37061234579';
   }
 
 }
---
+++ src/app/shared/models/user-full.ts
@@ -0,0 +1,8 @@
+export class UserFull {
+  id: number;
+  email: string;
+  password: string;
+  fullName: string;
+  natIdNumber: string;
+  phoneNumber: string;
+}
